6c3a9a29c97fdd0279376b05fe6754015f27f60c
rkum <rathin.kumar@viewglass.com>
2017-10-15 19:16:12 -0700

    To Do List App
    This simple To Do List iOS App has two view controllers and can be used to add and remove to do items

---
+++ ToDoList/ToDoList/SecondViewController.swift
@@ -0,0 +1,69 @@
+//
+//  SecondViewController.swift
+//  ToDoList
+//
+//  Created by Rathin Kumar on 10/14/17.
+//  Copyright © 2017 Rathin Kumar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var itemTextField: UITextField!
+    
+    
+    @IBAction func addButton(_ sender: Any) {
+        
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        var items:[String]
+        
+        if let tempItems = itemsObject as? [String] {
+            
+            items = tempItems
+            
+            items.append(itemTextField.text!)
+            
+            print(items)
+            
+        }
+        
+        else {
+            
+            items = [itemTextField.text!]
+        }
+        UserDefaults.standard.set(items, forKey: "items")
+        
+        itemTextField.text = ""
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+        
+    }
+        
+    
+        
+        
+        
+        
+  
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
